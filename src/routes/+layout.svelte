57d524e6962af38c31252f6a3791069bec08e14e
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-09-06 18:20:34 +0530

    added icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,24 @@
 	import '../app.postcss';
 </script>
 
-<div class="flex h-screen">
+<div class="flex h-screen font-sans font-bold">
 	
-	<div class="sideBar basis-1/3 my-2 mx-2 flex flex-col h-full">
+	<div class="sideBar basis-1/4 my-2 mx-2 flex flex-col ">
 		
-		<div class="sideBarOptions bg-surface-700 basis-1/6 mb-2">
+		<div class="sideBarOptions bg-surface-700 basis-1/6 mb-2 rounded-lg flex flex-col">
 			sidebar Options
+			<div class="p-2">
+				<a class="flex h-5 gap-4" href="">
+					<img class="dark:invert" src="/home-icon.png" alt=""><p class="font-1">Home</p>
+				</a>
+			</div>
+			<div>
+				Search
+			</div>
+
 		</div>
 
-		<div class="sideBarLibrary bg-surface-700 basis-5/6">
+		<div class="sideBarLibrary bg-surface-700 basis-5/6 rounded-lg">
 			sidebar Library
 		</div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<div class="mainDiv bg-surface-700 basis-2/3 my-2 ">
+<div class="mainDiv bg-surface-700 basis-3/4 my-2 me-2 rounded-lg">
 this is the mainDiv
 </div>
