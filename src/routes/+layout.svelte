3676b552234d55a1566191adf4646371906fbc5b
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-09-06 17:53:57 +0530

    first commit almost

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="flex h-screen">
+	
+	<div class="sideBar basis-1/3 my-2 mx-2 flex flex-col h-full">
+		
+		<div class="sideBarOptions bg-surface-700 basis-1/6 mb-2">
+			sidebar Options
+		</div>
+
+		<div class="sideBarLibrary bg-surface-700 basis-5/6">
+			sidebar Library
+		</div>
+
+	</div>
+	
+	<slot class="basis-2/3"/> 
+
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="mainDiv bg-surface-700 basis-2/3 my-2 ">
+this is the mainDiv
+</div>
